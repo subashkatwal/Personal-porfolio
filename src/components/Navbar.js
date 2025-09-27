276bc0d1c23b1f44a9b23f615027171197a9c265
subashkatwal <subashkatwal112@gmail.com>
2025-09-27 23:03:24 +0545

    Initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,53 @@ import "./Navbar.css";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
     <nav className="navbar">
       <div className="logo">Subash Katwal</div>
-      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
-        <li><Link to="home" smooth={true} duration={500} offset={-70}>Home</Link></li>
-        <li><Link to="about" smooth={true} duration={500} offset={-70}>About</Link></li>
-        <li><Link to="education" smooth={true} duration={500} offset={-70}>Education</Link></li>
-        <li><Link to="projects" smooth={true} duration={500} offset={-70}>Projects</Link></li>
-        <li><Link to="skills" smooth={true} duration={500} offset={-70}>Skills</Link></li>
-        <li><Link to="contact" smooth={true} duration={500} offset={-70}>Contact</Link></li>
-      </ul>
 
+      {/* Hamburger first (mobile) */}
       <div className="hamburger" onClick={toggleMenu}>
         <div></div>
         <div></div>
         <div></div>
       </div>
+
+      {/* Nav links */}
+      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+        <li>
+          <Link to="home" smooth={true} duration={500} offset={-70} onClick={toggleMenu}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500} offset={-70} onClick={toggleMenu}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="education" smooth={true} duration={500} offset={-70} onClick={toggleMenu}>
+            Education
+          </Link>
+        </li>
+        <li>
+          <Link to="projects" smooth={true} duration={500} offset={-70} onClick={toggleMenu}>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500} offset={-70} onClick={toggleMenu}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500} offset={-70} onClick={toggleMenu}>
+            Contact
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
